ecd6c0b86e1b6c603e0000c5d00479e168b2d73c
Jon Pero <github@jonpero.com>
2025-03-05 11:03:57 -0500

    improvements
    - optimized Image object usage and disposal
    - progress inidicators
    - error handling

--- powershell/images2vid.ps1
+++ powershell/images2vid.ps1
@@ -19,6 +19,14 @@ function ImageCheck {
     $pngFiles = Get-ChildItem -Path $FolderPath -Filter *.png -Recurse -File
     $jpgFiles = Get-ChildItem -Path $FolderPath -Filter *.jpg -Recurse -File
 
+    Write-Output "pngFiles: $($pngFiles.Count), jpgFiles: $($jpgFiles.Count)"
+
+    # if there are less than 2 files, exit
+    if ($pngFiles.Count -lt 2 -and $jpgFiles.Count -lt 2) {
+        Write-Warning "The folder contains less than 2 .png or .jpg files. Exiting..."
+        exit 1
+    }
+
     # Check for both .png and .jpg files, output a warning and exit if found
     if ($pngFiles -and $jpgFiles) {
         Write-Warning "The folder contains both .png and .jpg files. Exiting..."
@@ -28,9 +36,11 @@ function ImageCheck {
     } elseif ($jpgFiles) {
         #Write-Output "The folder contains .jpg files only."
         $ffmpegArgs[5] = "%05d.jpg"
-    } else {
+    }
+    else {
         Write-Output "The folder contains no .png or .jpg files."
     }
+    
 }
 
 # calculates the optimal width for all image files
@@ -38,11 +48,17 @@ function GetOptimalImageSize {
     # Get all PNG / JPG files in the folder
     $imageFiles = Get-ChildItem -Path $FolderPath -Recurse -Include "*.png","*.jpg"
 
+    $i = 0
+    $totalFiles = $imageFiles.Count
+    $progress = 0
+
     $maxWidth = 1
+    $image = New-Object System.Drawing.Bitmap 1, 1 # Create a reusable image object
     foreach ($imageFile in $imageFiles) {
         try {
-            # Load the image file
+            # Load the image file into the reusable image object
             $image = [System.Drawing.Image]::FromFile($imageFile.FullName)
+            $i++
 
             # Check if the image is loaded
             if ($image -eq $null) {
@@ -52,14 +68,21 @@ function GetOptimalImageSize {
 
             # Get dimensions
             $width = $image.Width
-            $height = $image.Height
+            #$height = $image.Height
 
-            Write-Host "  $($imageFile.Name) @ $($width)x$($height)"
+            #Write-Host "  $($imageFile.Name) @ $($width)x$($height)"
 
             # Update the optimal if the current is greater
             if ($width -gt $maxWidth) {
                 $maxWidth = $width
             }
+
+            # Dispose of the image to free memory
+            $image.Dispose()
+
+            # Update progress bar
+            $progress = [math]::Round(($i / $totalFiles) * 100)
+            Write-Progress -Activity "Reading Image Sizes" -Status "$progress% Complete" -PercentComplete $progress
         } catch {
             Write-Error "Error: Exception while processing $($imageFile.FullName) $($_.Exception.Message)"
         }
@@ -78,15 +101,24 @@ function ProcessImages {
 
     # Create a staging folder to store renamed image files
     $stagingFolderPath = Join-Path -Path $FolderPath -ChildPath "staging"
+    if (Test-Path $stagingFolderPath) {
+        Remove-Item -Path $stagingFolderPath -Recurse -Force
+    }
     New-Item -Path $stagingFolderPath -ItemType Directory -Force
 
-    # Rename and copy image files to the staging folder
-    $i = 1
+    $i = 0
+    $totalFiles = $fileList.Count
+    $progress = 0
+
     foreach ($file in $fileList) {
         $newName = ("{0:d5}" -f $i) + $file.Extension
         $destination = Join-Path -Path $stagingFolderPath -ChildPath $newName
         Copy-Item $file.FullName -Destination $destination
         $i++
+
+        # Update progress bar
+        $progress = [math]::Round(($i / $totalFiles) * 100)
+        Write-Progress -Activity "Copying Images" -Status "$progress% Complete" -PercentComplete $progress
     }
 
     # Run ffmpeg to create the video in the staging folder
@@ -100,10 +132,31 @@ function ProcessImages {
         Write-Output "ffmpeg.exe completed successfully."
     }
 
+    # generate a new name for the output file based on the folder name
+    $outputFileName = (Get-Item $FolderPath).Name + ".mp4"
+    
+    # rename and relocate the output file to the original folder
+    $outputFilePath = Join-Path -Path $FolderPath -ChildPath $outputFileName
+
+    # write message to console
+    Write-Output "Moving output file to $outputFilePath"
+
+    Move-Item -Path "$stagingFolderPath\output.mp4" -Destination $outputFilePath -Force
+
+    # Remove the staging folder
+    Remove-Item -Path $stagingFolderPath -Recurse -Force
+
 }
 
 # Verify the folder path exists before running the functions
 if (Test-Path $FolderPath) {
+    # Check if there are any image files in the folder
+    $imageFiles = Get-ChildItem -Path $FolderPath -Recurse -Include "*.png","*.jpg"
+    if ($imageFiles.Count -eq 0) {
+        Write-Error "The folder contains no .png or .jpg files. Exiting..."
+        exit 1
+    }
+
     ImageCheck
     $optimalWidth = GetOptimalImageSize
     #ensure we have a width
